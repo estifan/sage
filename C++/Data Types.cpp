bd0d1c83a878fe124fe8c07c8d771bb6c59faa5a
W <danwondimu00@gmail.com>
2025-07-09 13:52:24 +0300

    c++ Data Types

---
+++ C++/Data Types.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int age;
+	age = 25;
+	string name = "Estif";
+	cout<<"My name is: "<<name<<endl;
+	cout<<"My age is: "<<age<<endl;
+	
+	return 1;
+}
